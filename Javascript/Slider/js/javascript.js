41ae9bd2fb4483a11239aab49d2691579aeaffeb
hoangtho123456 <danghoangtho1994@gmail.com>
2017-09-25 10:15:23 +0700

    update js comment for bt_slide and calender

--- Javascript/Slider/js/javascript.js
+++ Javascript/Slider/js/javascript.js
@@ -1,15 +1,24 @@
-var slideIndex = 0;
-var reTimeout;
-showDivs(slideIndex);
+/**
+*Create the animation for lides in HTML
+*@author danghoangtho1132@gmail.com (Đặng Hoàng Thọ)
+*/
+var slideIndex = 0; //@slideIndex is a value it increase or decrease follow the change of slides
+var reTimeout; //@reTimeout is a value will reset time of a slide if you pick another slides
+showDivs(slideIndex); //function show slide animation.
 
+/**
+*when you click button ">" or "<":
+*@value slideIndex will increase (or decrease), 
+  the current slide move the next (or move the before slide).
+*/
 function plusDivs(n) {
 	showDivs(slideIndex += n);
 }
-
+//Show the slide is corresponding to the selected bar shape
 function currentDiv(n) {
 	showDivs(slideIndex = n - 1);
 }
-
+//show slide animation
 function showDivs(n) {
 	var i;
 	var slide = document.getElementsByClassName("mySlides");
@@ -19,12 +28,12 @@ function showDivs(n) {
 	for (i = 0; i < slide.length; i++) {
 		slide[i].style.display = "none";
 	}
-	//dots
+	//dots is the img bar to choose the img
 	for (i = 0; i < dots.length; i++) {
 		dots[i].className = dots[i].className.replace(" sl-opacity-off", "");
 	}
 	slide[slideIndex].style.display = "block";
-	dots[slideIndex].className += " sl-opacity-off";
+	dots[slideIndex].className += " sl-opacity-off"; 
 	//use clearTimeout to reset the time of the slide is running.
 	clearTimeout(reTimeout);
 	reTimeout = setTimeout(function () { showDivs(slideIndex += 1); }, 5000);

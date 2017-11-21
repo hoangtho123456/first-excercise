a3af04740b4e5b9d2a07cb7be8d63cd5b6c17d08
hoangtho123456 <danghoangtho1994@gmail.com>
2017-11-21 17:12:47 +0700

    update bt 3d chart

--- designpattern/3d-pie-chart/js/javascript.js
+++ designpattern/3d-pie-chart/js/javascript.js
@@ -95,7 +95,7 @@ var chart = (function () {
 
         width = percent < 0.5 ? 110 : -110; //if percent < 0.5, line and text will lay on the right oh screen, or reverse
         var curX = canvas.width / 3 + (radius / 2) * Math.cos(start_angle + end_angle / 2) + 100;
-        var curY = canvas.width / 3 + (radius / 2) * Math.sin(start_angle + end_angle / 2) * yScale;
+        var curY = canvas.width / 3 + (radius / 2) * Math.sin(start_angle + end_angle / 2) * yScale - 10;
         privateDrawLine(curX, curY, curX + width, curY - 70, width, "brown");
         ctx.fillStyle = "#000";
         ctx.font = "17px Arial";

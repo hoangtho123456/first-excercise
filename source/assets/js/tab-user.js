738819622a0758c40df0f31767c12824c6b530b9
hoangtho123456 <danghoangtho1994@gmail.com>
2018-01-08 20:14:18 +0700

    fix test show in tab user

--- source/assets/js/tab-user.js
+++ source/assets/js/tab-user.js
@@ -13,27 +13,28 @@ var DETAIL_INFO = $(".detail-info-js span");
 $(document).ready(function () {
 	var i = 0;
 
-	for (i = 0; i < TAB.length; i++) {
+  for (i = 0; i < TAB.length; i++) {
     if(TAB.eq(i).hasClass("active")) {
-        DIV.eq(i).fadeIn();
+      DIV.eq(i).fadeIn();
+      DETAIL_INFO.hide();
     } else {
       DIV.eq(i).hide();
     } 
-	}
+  }
 });
 
 TAB.click(function () {
-	var item = TAB.index(this);
+  var item = TAB.index(this);
   var i = 0;
 
-	for (i = 0; i < TAB.length; i++) {
-      TAB.eq(i).removeClass("active");
-      DIV.eq(i).hide();
-      DETAIL_INFO.hide();
-	}
-	if(item != 0) {
-      DETAIL_INFO.show();
-	}
-	TAB.eq(item).addClass("active");
-	DIV.eq(item).fadeIn();
+  for (i = 0; i < TAB.length; i++) {
+    TAB.eq(i).removeClass("active");
+    DIV.eq(i).hide();
+    DETAIL_INFO.hide();
+  }
+  if(item != 0) {
+    DETAIL_INFO.show();
+  }
+  TAB.eq(item).addClass("active");
+  DIV.eq(item).fadeIn();
 });

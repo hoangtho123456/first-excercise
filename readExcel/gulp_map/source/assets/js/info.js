3a898a598c8bf47df637bc0afc55c8763dc4ae14
hoangtho123456 <danghoangtho1994@gmail.com>
2018-01-22 10:27:39 +0700

    upgrade structure project

--- readExcel/gulp_map/source/assets/js/info.js
+++ readExcel/gulp_map/source/assets/js/info.js
@@ -1,69 +1,132 @@
 var COUNTRY_NAME = $('.country-name-js');
 var LIST_INFO = $('.info-p-tit');
+var BTN_COMPARE = $('.info-compare-js');
+var ARR_JA = [];
 
+var INFO_JA1 = $('#infoFirst_1');
+var INFO_JA2 = $('#infoFirst_2');
+var INFO_JA3 = $('#infoFirst_3');
+var INFO_JA4 = $('#infoFirst_4');
+/*=========Event handler=========*/
 $(document).ready(function() {
-  showListInfo();	
+  showListInfo();
+  showInfoJa();
+  console.log(ARR_JA);	
 });
 
-function showListInfo() {
-  if (navigator.cookieEnabled) {
-    COUNTRY_NAME.text(getCookie("nation"));
+$(BTN_COMPARE).on('click', function () {
+  if(typeof(Storage) !== "undefined") {
+    if(INFO_JA1.text() === "") {
+      INFO_JA1.text("/" + ARR_JA[0][2]);
+    } else {
+      INFO_JA1.text("");
+    }
 
+    if (INFO_JA2.text() === "") {
+      INFO_JA2.text("/" + ARR_JA[0][3]);
+    } else {
+      INFO_JA2.text("");
+    }
+
+    if (INFO_JA3.text() === "") {
+      INFO_JA3.text("/" + ARR_JA[0][4] + " " + ARR_JA[0][5]);
+    } else {
+      INFO_JA3.text("");
+    }
+
+    if (INFO_JA4.text() === "") {
+      INFO_JA4.text("/" + ARR_JA[0][4] + " " + ARR_JA[0][5]);
+    } else {
+      INFO_JA4.text("");
+    }    
+  }
+});
+
+/*=========function=========*/
+function showInfoJa() {
+  for (var key in RESULT) {
+    if (RESULT.hasOwnProperty(key)) {
+      for (i = 3; i < RESULT[key].length; i++) {
+        if (RESULT[key][i][1] !== undefined && RESULT[key][i][1] == "日本国") {
+          ARR_JA.push(RESULT[key][i]);
+        }
+      }
+    }
+  }
+}
+
+function showListInfo() {
+  if(typeof(Storage) !== 'undefined') {
+    COUNTRY_NAME.text(sessionStorage.getItem("nation"));
+    //sessionStorage.removeItem("nation");
+    
     for (var i = 0; i < LIST_INFO.length; i++) {
-      switch (LIST_INFO.eq(i).text()) {
+      switch(LIST_INFO.eq(i).text()) {
         case '州:':
-          LIST_INFO.eq(i).next().text(getCookie("州"));
-          setCookie("州", '', -1);
+          if(sessionStorage.getItem("州") !== null) {
+            LIST_INFO.eq(i).next().text(sessionStorage.getItem("州"));
+          } else {
+            LIST_INFO.eq(i).css("display", "none");
+          }
+          //LIST_INFO.eq(i).next().text(sessionStorage.getItem("州"));
+          //sessionStorage.removeItem("州");
           break;
         case '首都:':
-          LIST_INFO.eq(i).next().text(getCookie("首都"));
-          setCookie("首都", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("首都"));
+          //sessionStorage.removeItem("首都");
           break;
         case '面積:':
-          LIST_INFO.eq(i).next().text(getCookie("面積"));
-          setCookie("面積", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("面積"));
+          //sessionStorage.removeItem("面積");
           break;
         case '人口:':
-          LIST_INFO.eq(i).next().text(getCookie("人口"));
-          setCookie("人口", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("人口"));
+          //setCookie("人口", '', -1);
+          //sessionStorage.removeItem("人口");
           break;
         case '平均寿命:':
-          LIST_INFO.eq(i).next().text(getCookie("平均寿命"));
-          setCookie("平均寿命", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("平均寿命"));
+          //sessionStorage.removeItem("平均寿命");
           break;
         case '主な言語:':
-          LIST_INFO.eq(i).next().text(getCookie("主な言語"));
-          setCookie("主な言語", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("主な言語"));
+          //sessionStorage.removeItem("主な言語");
           break;
         case '主な宗教:':
-          LIST_INFO.eq(i).next().text(getCookie("主な宗教"));
-          setCookie("主な宗教", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("主な宗教"));
+          //sessionStorage.removeItem("主な宗教");
           break;
         case '通貨:':
-          LIST_INFO.eq(i).next().text(getCookie("通貨"));
-          setCookie("通貨", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("通貨"));
+          //sessionStorage.removeItem("通貨");
           break;
         case '伝統的な料理:':
-          LIST_INFO.eq(i).next().text(getCookie("伝統的な料理"));
-          setCookie("伝統的な料理", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("伝統的な料理"));
+          //sessionStorage.removeItem("伝統的な料理");
           break;
         case '国・地域の旗について:':
-          LIST_INFO.eq(i).next().text(getCookie("国・地域の旗について"));
-          setCookie("国・地域の旗について", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("国・地域の旗について"));
+          //sessionStorage.removeItem("国・地域の旗について");
           break;
         case 'さかんなスポーツ:':
-          LIST_INFO.eq(i).next().text(getCookie("さかんなスポーツや国技"));
-          setCookie("さかんなスポーツや国技", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("さかんなスポーツや国技"));
+          //sessionStorage.removeItem("さかんなスポーツや国技");
           break;
         case '過去のオリンピック開催都市:':
-          LIST_INFO.eq(i).next().text(getCookie("過去のオリンピック開催都市"));
-          setCookie("過去のオリンピック開催都市", '', -1);
+          LIST_INFO.eq(i).next().text(sessionStorage.getItem("過去のオリンピック開催都市"));
+          //sessionStorage.removeItem("過去のオリンピック開催都市");
           break;
         case '2016年リオオリンピックでのメダルかくとく数:':
-          LIST_INFO.eq(i).next().text(getCookie("2016年リオオリンピックでのメダルかくとく数"));
-          setCookie("2016年リオオリンピックでのメダルかくとく数", '', -1);
+          if(sessionStorage.getItem("2016年リオオリンピックでのメダルかくとく数") !== null) {
+            LIST_INFO.eq(i).next().text(sessionStorage.getItem("2016年リオオリンピックでのメダルかくとく数"));
+            //sessionStorage.removeItem("2016年リオオリンピックでのメダルかくとく数");
+          } else {
+            LIST_INFO.eq(i).css("display", "none");
+          }
+          //LIST_INFO.eq(i).next().text(sessionStorage.getItem("2016年リオオリンピックでのメダルかくとく数"));
+          //sessionStorage.removeItem("2016年リオオリンピックでのメダルかくとく数");
           break;
       }
     }
-  } else { alert("please start cookie to get data!"); }
+  }
 }
--- readExcel/gulp_map/source/assets/js/js.js
+++ readExcel/gulp_map/source/assets/js/js.js
@@ -16,7 +16,13 @@ var CHECKBOX10 = $("#data_check10");
 var CHECKBOX11 = $("#data_check11");
 var CHECKBOX12 = $("#data_check12");
 
-var SELECT = document.getElementsByTagName("select");
+var CONTINENTS = $("#continents");
+var NATION_SEARCH = $("#search");
+var NATIONS = $('#nations');
+var SEARCH = $('#search');
+//Get data from excel
+var RESULT = {};
+
 /* set up async GET request */
 var oReq;
 if (window.XMLHttpRequest) {
@@ -29,98 +35,22 @@ oReq.open("GET", url, true);
 oReq.responseType = "arraybuffer";
 
 oReq.onload = function() {
-  //var NATION_SELECT = $("#nations");
-  var CONTINENTS = $("#continents");
-  var NATION_SEARCH = $("#search");
-  var NATIONS = $('#nations');
-  var SEARCH = $('#search');
   var arraybuffer = oReq.response;
 
   var data = new Uint8Array(arraybuffer);
   var workbook = XLSX.read(data, { type: "array" });
 
-  //Get data from excel
-  var result = {};
   workbook.SheetNames.forEach(function(sheetName) {
     var roa = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName], { header: 1 });
     if (roa.length > 0) {
-      result[sheetName] = roa;
+      RESULT[sheetName] = roa;
     }
   });
 
-  //get key name of object result
-  var keyName = Object.keys(result);
+  //get key name of object RESULT
+  var keyName = Object.keys(RESULT);
 
-  //show list name of continents
-  for (var i = 0; i < keyName.length; i++) {
-    CONTINENTS.append("<option value='" + keyName[i] + "'>" + keyName[i] + "</option>");
-  }
-
-  for (var key in result) {
-    if (result.hasOwnProperty(key)) {
-      for (i = 3; i < result[key].length; i++) {
-        if (result[key][i][1] !== undefined) {
-          NATIONS.append("<option value='" + result[key][i][1] + "'>" + result[key][i][1] + "</option>");
-          arr.push(result[key][i][1]);
-        }
-      }
-    }
-  }
-
-  /*
-   *click pick any continents will show corresponding data nation
-   */
-  $("#continents option").on('click', function() {
-    NATIONS.html("");
-    var item = $(this).val();
-    for (var key in result) {
-      if (result.hasOwnProperty(key)) {
-        if (item === key) {
-          for (i = 3; i < result[key].length; i++) {
-            if (result[key][i][1] !== undefined) {
-              NATIONS.append("<option value='" + result[key][i][1] + "'>" + result[key][i][1] + "</option>");
-            }
-          }
-        }
-      }
-    }
-  });
-
-  BTN_SUBMIT.on('click', function() {
-    var country = '';
-    if (!NATION_SELECT.attr('disabled')) {
-      country = NATION_SELECT.val();
-
-      for (var key in result) {
-        if (result.hasOwnProperty(key)) {
-          for (i = 3; i < result[key].length; i++) {
-            if (result[key][i][1] === country) {
-              setCookie("nation", result[key][i][1], -1);
-              LIST_DATA = result[key][i];
-              console.log(LIST_DATA);
-              break;
-            }
-          }
-        }
-      }
-    } else {
-      country = NATION_SEARCH.val();
-      for (var key in result) {
-        if (result.hasOwnProperty(key)) {
-          for (i = 3; i < result[key].length; i++) {
-            if (result[key][i][1] === country) {
-              setCookie("nation", result[key][i][1], -1);
-              LIST_DATA = result[key][i];
-              console.log(LIST_DATA);
-              break;
-            }
-          }
-        }
-      }
-    }
-    clickCheckBox();
-    window.open('info.html', '_self');
-  });
+  clickSelect(keyName);
 
   /*
    *click pick any continents will show corresponding data nation
@@ -128,117 +58,198 @@ oReq.onload = function() {
   $(SEARCH).autocomplete({
     source: [arr]
   });
-  //console.log(arr);
 }
 
 oReq.send();
 
+/*
+ *click pick any continents will show corresponding data nation
+ */
+$("#continents").on('click', function() {
+  NATIONS.html("");
+  var item = $(this).val();
+  for (var key in RESULT) {
+    if (RESULT.hasOwnProperty(key)) {
+      if (item === key) {
+        for (i = 3; i < RESULT[key].length; i++) {
+          if (RESULT[key][i][1] !== undefined) {
+            NATIONS.append("<option value='" + RESULT[key][i][1] + "'>" + RESULT[key][i][1] + "</option>");
+          }
+        }
+      }
+    }
+  }
+});
+
+BTN_SUBMIT.on('click', function() {
+  var country = '';
+  if (!NATION_SELECT.attr('disabled')) {
+    country = NATION_SELECT.val();
+
+    for (var key in RESULT) {
+      if (RESULT.hasOwnProperty(key)) {
+        for (i = 3; i < RESULT[key].length; i++) {
+          if (RESULT[key][i][1] === country) {
+            sessionStorage.setItem("nation", RESULT[key][i][1]);
+            LIST_DATA = RESULT[key][i];
+            console.log(LIST_DATA);
+            break;
+          }
+        }
+      }
+    }
+  } else {
+    country = NATION_SEARCH.val();
+    for (var key in RESULT) {
+      if (RESULT.hasOwnProperty(key)) {
+        for (i = 3; i < RESULT[key].length; i++) {
+          if (RESULT[key][i][1] === country) {
+            sessionStorage.setItem("nation", RESULT[key][i][1]);
+            LIST_DATA = RESULT[key][i];
+            console.log(LIST_DATA);
+            break;
+          }
+        }
+      }
+    }
+  }
+  clickCheckBox();
+  window.open('info.html', '_self');
+});
+
+/*==========function==========*/
+/*
+ * throw data nations and continents into select tags
+ */
+function clickSelect(keyName) {
+  //show list name of continents
+  for (var i = 0; i < keyName.length; i++) {
+    CONTINENTS.append("<option value='" + keyName[i] + "'>" + keyName[i] + "</option>");
+  }
+
+  for (var key in RESULT) {
+    if (RESULT.hasOwnProperty(key)) {
+      for (i = 3; i < RESULT[key].length; i++) {
+        if (RESULT[key][i][1] !== undefined) {
+          NATIONS.append("<option value='" + RESULT[key][i][1] + "'>" + RESULT[key][i][1] + "</option>");
+          arr.push(RESULT[key][i][1]);
+        }
+      }
+    }
+  }
+}
+
 function clickCheckBox() {
-  if (CHECKBOX1.prop("checked")) {
-    if(LIST_DATA[2] !== undefined) {
-      setCookie("州", LIST_DATA[2], -1);
-      console.log(LIST_DATA[2]);
+  if (typeof(Storage) !== 'undefined') {
+    if (CHECKBOX1.prop("checked")) {
+      if (LIST_DATA[2] !== undefined) {
+        sessionStorage.setItem("州", LIST_DATA[2]);
+      }
+    } else {
+      sessionStorage.removeItem("州");
     }
-  } else {
-    setCookie("州", '', -1);
-  }
 
-  if (CHECKBOX2.prop("checked")) {
-    if (LIST_DATA[3] !== undefined) {
-      setCookie("首都", LIST_DATA[3], -1);
-      console.log(LIST_DATA[3]);
+    if (CHECKBOX2.prop("checked")) {
+      if (LIST_DATA[3] !== undefined) {
+        sessionStorage.setItem("首都", LIST_DATA[3]);
+        console.log(LIST_DATA[3]);
+      }
+    } else {
+      //setCookie("首都", '', -1);
+      sessionStorage.removeItem("首都");
     }
-  } else {
-    setCookie("首都", '', -1);
-  }
 
-  if(CHECKBOX3.prop("checked")) {
-    if (LIST_DATA[4] !== undefined && LIST_DATA[5] !== undefined) {
-      setCookie("面積", LIST_DATA[4] + " " + LIST_DATA[5], -1);
-      console.log(LIST_DATA[4] + " " + LIST_DATA[5]);
-    } 
-  } else {
-    setCookie("面積", '', -1);
-  } 
-
-  if(CHECKBOX4.prop("checked")) {
-    if (LIST_DATA[6] !== undefined && LIST_DATA[7] !== undefined) {
-      setCookie("人口", LIST_DATA[6] + " " + LIST_DATA[7], -1);
-      console.log(LIST_DATA[6] + " " + LIST_DATA[7]);
-    }  
-  } else {
-    setCookie("人口", '', -1);
-  }
-
-  if(CHECKBOX5.prop("checked")) {
-    if(LIST_DATA[8] !== undefined) {
-      setCookie("平均寿命", LIST_DATA[8], -1);
-      console.log(LIST_DATA[8]);
+    if (CHECKBOX3.prop("checked")) {
+      if (LIST_DATA[4] !== undefined && LIST_DATA[5] !== undefined) {
+        sessionStorage.setItem("面積", LIST_DATA[4] + " " + LIST_DATA[5]);
+        console.log(LIST_DATA[4] + " " + LIST_DATA[5]);
+      }
+    } else {
+      sessionStorage.removeItem("面積");
     }
-  } else {
-    setCookie("平均寿命", '', -1);
+
+    if (CHECKBOX4.prop("checked")) {
+      if (LIST_DATA[6] !== undefined && LIST_DATA[7] !== undefined) {
+        sessionStorage.setItem("人口", LIST_DATA[6] + " " + LIST_DATA[7]);
+        console.log(LIST_DATA[6] + " " + LIST_DATA[7]);
+      }
+    } else {
+      sessionStorage.removeItem("人口");
+    }
+
+    if (CHECKBOX5.prop("checked")) {
+      if (LIST_DATA[8] !== undefined) {
+        sessionStorage.setItem("平均寿命", LIST_DATA[8]);
+        console.log(LIST_DATA[8]);
+      }
+    } else {
+      sessionStorage.removeItem("平均寿命");
+    }
+
+    if (CHECKBOX6.prop("checked")) {
+      if (LIST_DATA[9] !== undefined) {
+        sessionStorage.setItem("主な宗教", LIST_DATA[9]);
+        console.log(LIST_DATA[9]);
+      }
+    } else {
+      sessionStorage.removeItem("主な宗教");
+    }
+
+    if (CHECKBOX7.prop("checked")) {
+      if (LIST_DATA[14] !== undefined) {
+        sessionStorage.setItem("国・地域の旗について", LIST_DATA[14]);
+        console.log(LIST_DATA[14] === undefined ? '' : LIST_DATA[14]);
+      }
+    } else {
+      sessionStorage.removeItem("国・地域の旗について");
+    }
+
+    if (CHECKBOX8.prop("checked")) {
+      if (LIST_DATA[12] !== undefined) {
+        //setCookie("通貨", LIST_DATA[12], -1);
+        sessionStorage.setItem("通貨", LIST_DATA[12]);
+        console.log(LIST_DATA[12]);
+      }
+    } else {
+      sessionStorage.removeItem("通貨");
+    }
+
+    if (CHECKBOX9.prop("checked")) {
+      if (LIST_DATA[13] !== undefined) {
+        sessionStorage.setItem("伝統的な料理", LIST_DATA[13]);
+        console.log(LIST_DATA[13]);
+      }
+    } else {
+      sessionStorage.removeItem("伝統的な料理");
+    }
+
+    if (CHECKBOX10.prop("checked")) {
+      if (LIST_DATA[15] !== undefined) {
+        sessionStorage.setItem("さかんなスポーツや国技", LIST_DATA[15]);
+        console.log(LIST_DATA[15]);
+      }
+    } else {
+      sessionStorage.removeItem("さかんなスポーツや国技");
+    }
+
+    if (CHECKBOX11.prop("checked")) {
+      if (LIST_DATA[16] !== undefined) {
+        sessionStorage.setItem("過去のオリンピック開催都市", LIST_DATA[16]);
+        console.log(LIST_DATA[16]);
+      }
+    } else {
+      sessionStorage.removeItem("過去のオリンピック開催都市");
+    }
+
+    if (CHECKBOX12.prop("checked")) {
+      if (LIST_DATA[17] !== undefined && LIST_DATA[19] !== undefined && LIST_DATA[21] !== undefined) {
+        sessionStorage.setItem("2016年リオオリンピックでのメダルかくとく数", LIST_DATA[17] + 
+                              " " + LIST_DATA[19] + " " + LIST_DATA[21]);
+        console.log(LIST_DATA[17] + " " + LIST_DATA[19] + " " + LIST_DATA[21]);
+      }
+    } else {
+      sessionStorage.removeItem("2016年リオオリンピックでのメダルかくとく数");
+    }
   }
 
-  if(CHECKBOX6.prop("checked")) {
-    if(LIST_DATA[9] !== undefined) {
-      setCookie("主な宗教", LIST_DATA[9], -1);
-      console.log(LIST_DATA[9]);
-    }
-  } else {
-    setCookie("主な宗教", '', -1);
-  }
-
-  if(CHECKBOX7.prop("checked")) {
-    if(LIST_DATA[14] !== undefined) {
-      setCookie("国・地域の旗について", LIST_DATA[14], -1);
-      console.log(LIST_DATA[14] === undefined?'': LIST_DATA[14]);
-    }
-  } else {
-    setCookie("国・地域の旗について", '', -1);
-  }
-
-  if(CHECKBOX8.prop("checked")) {
-    if(LIST_DATA[12] !== undefined) {
-      setCookie("通貨", LIST_DATA[12], -1);
-      console.log(LIST_DATA[12]);
-    }
-  } else {
-    setCookie("通貨", '', -1);
-  } 
-
-  if (CHECKBOX9.prop("checked")) {
-    if(LIST_DATA[13] !== undefined) {
-      setCookie("伝統的な料理", LIST_DATA[13], -1);
-      console.log(LIST_DATA[13]);
-    }
-  } else {
-    setCookie("伝統的な料理", '', -1);
-  }
-  if(CHECKBOX10.prop("checked")) {
-    if(LIST_DATA[15] !== undefined) {
-      setCookie("さかんなスポーツや国技", LIST_DATA[15], -1);
-      console.log(LIST_DATA[15]);
-    }
-  } else {
-    setCookie("さかんなスポーツや国技", '', -1);
-  }
-
-  if (CHECKBOX11.prop("checked")) {
-    if(LIST_DATA[16] !== undefined) {
-      setCookie("過去のオリンピック開催都市", LIST_DATA[16], -1);
-      console.log(LIST_DATA[16]);
-    } 
-  } else {
-    setCookie("過去のオリンピック開催都市", '', -1);
-  }
-
-  if (CHECKBOX12.prop("checked")) {
-    if(LIST_DATA[17] !== undefined && LIST_DATA[19] !== undefined && LIST_DATA[21] !== undefined) {
-      setCookie("2016年リオオリンピックでのメダルかくとく数", LIST_DATA[17] + " " + LIST_DATA[19] + " " + LIST_DATA[21], -1);
-      console.log(LIST_DATA[17] + " " + LIST_DATA[19] + " " + LIST_DATA[21]);
-    }
-  } else {
-    setCookie("2016年リオオリンピックでのメダルかくとく数", '', -1);
-  }
-  
 }
